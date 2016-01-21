a42bbfba1d19c311738b6f2cb2a891cc3647c9f7
Daniel Hägglund <daniel.hagglund@doberman.se>
2016-01-21 17:45:24 +0100

    Some tests

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var pluginsutils = require('rollup-pluginutils');
 var msx = require('msx');
+var assign = require('object-assign');
 
-module.exports = function(opts) {
-  console.log('created msx transform');
-  var mithrilImport = 'import m from "mithril";';
-  var filter = pluginsutils.createFilter('**/*.msx');
-  var options = opts || undefined;
+module.exports = function(options) {
+  options = options || {};
+  // var mithrilImport = 'import m from "mithril";';
+  var ext = options.ext || 'msx';
+  var filter = pluginsutils.createFilter('**/*.' + ext);
+
+  options = assign({}, options);
+
+  // delete plugin options, leaving msx options
+  delete options.ext;
 
   return {
     transform: function(source, id) {
       if(!filter(id)) return null;
-      //HACK: commenting out imports and exports, because msx compiler cannot handle them. this needs testing
+      //HACK: commenting out imports and exports, because msx compiler cannot handle them.
       var fixedSource = source.replace(/import.*from.*;?|export\s+(?:default|{[^}]*})?/g, '/*--*$&*--*/');
       var code = msx.transform(fixedSource, options);
       //restoring the imports and exports.
       var fixedCode = code.replace(/\/\*--\*(.*)\*--\*\//g, '$1');
       return fixedCode;
     }
-  }
+  };
 };
---
+++ test/transform.spec.js
@@ -0,0 +1,134 @@
+/*jshint multistr: true */
+var expect = require('chai').expect;
+var plugin = require('../index');
+var path = require('path');
+
+function fakePath(filename) {
+  return path.join(process.cwd(), 'fakepath', filename);
+}
+
+describe('rollup-plugin-msx', function() {
+
+  describe('#transform()', function() {
+    var instance;
+
+    before(function() {
+      instance = plugin();
+    });
+
+    after(function() {
+      instance = null;
+    });
+
+    describe('import statements', function() {
+
+      it('should handle import something from "somewhere" statement', function() {
+        var source = 'import something from "./somewhere";\nsomething(function() { return <div>test</div>; });\n';
+        var code = instance.transform(source, fakePath('fakefile.msx'));
+        var expected = 'import something from "./somewhere";\nsomething(function() { return {tag: "div", attrs: {}, children: ["test"]}; });\n';
+        expect(code).to.equal(expected);
+      });
+
+      it('should handle import * as something from "somewhere" statement', function() {
+        var source = 'import * as something from "./somewhere";\nsomething.test(function() { return <div>test</div>; });\n';
+        var code = instance.transform(source, fakePath('fakefile.msx'));
+        var expected = 'import * as something from "./somewhere";\nsomething.test(function() { return {tag: "div", attrs: {}, children: ["test"]}; });\n';
+        expect(code).to.equal(expected);
+      });
+
+      it('should handle import { something } from "somewhere" statement', function() {
+        var source = 'import { something } from "./somewhere";\nsomething(function() { return <div>test</div>; });\n';
+        var code = instance.transform(source, fakePath('fakefile.msx'));
+        var expected = 'import { something } from "./somewhere";\nsomething(function() { return {tag: "div", attrs: {}, children: ["test"]}; });\n';
+        expect(code).to.equal(expected);
+      });
+
+    });
+
+    describe('export statements', function() {
+
+      it('should handle handle export default something statement', function() {
+        var source = 'function something() {\
+  return <div>test</div>;\
+}\
+export default something;\
+';
+        var code = instance.transform(source, fakePath('fakefile.msx'));
+        var expected = 'function something() {\
+  return {tag: "div", attrs: {}, children: ["test"]};\
+}\
+export default something;\
+';
+        expect(code).to.equal(expected);
+      });
+
+      it('should handle handle export default function something() { ... } statement', function() {
+        var source = 'export default function something() {\
+  return <div>test</div>;\
+}\
+';
+        var code = instance.transform(source, fakePath('fakefile.msx'));
+        var expected = 'export default function something() {\
+  return {tag: "div", attrs: {}, children: ["test"]};\
+}\
+';
+        expect(code).to.equal(expected);
+      });
+
+      it('should handle handle export something statement', function() {
+        var source = 'function something() {\
+  return <div>test</div>;\
+}\
+export something;\
+';
+        var code = instance.transform(source, fakePath('fakefile.msx'));
+        var expected = 'function something() {\
+  return {tag: "div", attrs: {}, children: ["test"]};\
+}\
+export something;\
+';
+        expect(code).to.equal(expected);
+      });
+
+      it('should handle handle export { something, somethingElse } statement', function() {
+        var source = 'function something() {\
+  return <div>test</div>;\
+}\
+var somethingElse = true;\
+export { something, somethingElse };\
+';
+        var code = instance.transform(source, fakePath('fakefile.msx'));
+        var expected = 'function something() {\
+  return {tag: "div", attrs: {}, children: ["test"]};\
+}\
+var somethingElse = true;\
+export { something, somethingElse };\
+';
+        expect(code).to.equal(expected);
+      });
+
+    });
+
+  });
+
+  describe('#transform(options)', function() {
+
+    describe('custom extension', function() {
+
+      it('should transform files with matching custom extension', function() {
+        var instance = plugin({ext: 'js'});
+        var code = instance.transform('export default function something() { return <div>test</div>; }', fakePath('fakefile.js'));
+        expect(code).to.not.be.null;
+      });
+
+      it('should not transform files with non-matching extension', function() {
+        var instance = plugin({ext: 'js'});
+        var code = instance.transform('export default function something() { return <div>test</div>; }', fakePath('fakefile.msx'));
+        expect(code).to.be.null;
+      });
+
+    });
+    
+  });
+
+});
